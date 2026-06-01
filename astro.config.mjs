31889b2841921d6c8617dc37ae0447ca05db8abf
pablo7843 <dewislewis8@gmail.com>
2026-06-01 15:07:20 +0200

    hybrid option Astro v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
   adapter: vercel({
     webAnalytics: { enabled: true }
   }),
